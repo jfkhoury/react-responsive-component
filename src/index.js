fdd965ded09ab37c6ea65044875ce81c250659d4
donavon <github@donavon.com>
2017-07-06 13:09:46 -0400

    export default as well

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import "matchmedia-polyfill";
 import "matchmedia-polyfill/matchMedia.addListener";
 import ResponsiveComponent from "./ui/ResponsiveComponent";
 
+export default ResponsiveComponent;
 export { ResponsiveComponent };
